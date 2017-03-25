923f8e7cee6059a1acc8c1693eadd7b37898a76f
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-03-25 17:32:50 +0000

    added controls for filtering by date, only for admins, not hooked up yet

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ $(document).ready(() => {
 
     //controls for filtering and printing all
     const $controlsForm = $('#controls-form')
-    const $printAllButton = $('#print-all-button')
 
     // cache header for logging in and out
     const $headerContainer =  $('#header-container-outer')
@@ -63,7 +62,7 @@ $(document).ready(() => {
 
     //delete one commendation
     $commendationsContainer.on('click', '.delete-btn', (event) => {
-      const $commendationToDelete =$(event.target).closest('.commendation')
+      const $commendationToDelete = $(event.target).closest('.commendation')
       const dataId = $commendationToDelete.attr('data-id')
       const uid = $commendationToDelete.attr('data-owner')
       console.log(`deleting: ${dataId}`)
@@ -74,7 +73,15 @@ $(document).ready(() => {
       })
     })
 
-    $printAllButton.on('click', () => {
+    $controlsForm.on('click', '#date-filter-button', (event) =>{
+      const $dateInput = $(event.target).prev()
+      const dateString = $dateInput.val()
+      filterByDate(dateString)
+      $dateInput.val('')
+      return false
+    })
+
+    $controlsForm.on('click', '#print-all-button', (event) => {
       printAllCommendations()
       return false
     })
@@ -92,17 +99,14 @@ $(document).ready(() => {
           console.log(`are you an admin?: ${isAdmin}`)
           makeCommendationRefsArray(isAdmin, user.uid)
           if(isAdmin){
-            $controlsForm.find('h2').text('All Commendations')
-          } else {
-            $controlsForm.find('h2').text('My Commendations')
+            renderControlsForm($controlsForm)
           }
-        })
+      })
         $commendationsForm.removeClass('hidden')
-        $controlsForm.removeClass('hidden')
       } else {
         removeAllCommendations($commendationsContainer)
+        removeControlsForm($controlsForm)
         $commendationsForm.addClass('hidden')
-        $controlsForm.addClass('hidden')
       }
       renderHeader(user)
     })
@@ -190,6 +194,22 @@ $(document).ready(() => {
       }
     }
 
+    //called by change in auth state if usr is an admin
+    function renderControlsForm($container){
+      const html = `
+        <h2>All Commendations:</h2>
+        <p>Display commendations since:</p>
+        <div id="date-filter-container">
+          <input type="date" id="date-filter">
+          <button type="button" id="date-filter-button">Filter</button>
+        </div>
+        <hr>
+        <button type="button" id="print-all-button">Print All</button>
+      `
+      $container.append(html)
+      $container.removeClass('hidden')
+    }
+
     //called when children are added to db reference
     function renderCommendation(snapshot){
       const val = snapshot.val()
@@ -207,6 +227,10 @@ $(document).ready(() => {
       </div>
       `
       $commendationsContainer.prepend(html)
+    }
+
+    function filterByDate(filterDate){
+      console.log(filterDate)
     }
 
     //called by the print buttons
@@ -252,12 +276,11 @@ $(document).ready(() => {
         window.print()
         $('.printing').remove()
       }, 250)
-
     }
 
     //called by the print buttons
     function printAllCommendations(event){
-      console.log('hello all')
+      let html = ''
       $('.commendation').each((index, value) => {
         const $curCommendation = $(value)
         const name = $curCommendation.find('.commendation-name').html()
@@ -268,7 +291,7 @@ $(document).ready(() => {
         //slice off word 'by':
         const by = $curCommendation.find('.commendation-by').html().slice(2)
         const printLogo = logoTemplate('#000000', 100)
-        const html = `
+        const templateHtml = `
         <div class="printing">
          <div class="banner">
            <div class="logo-left">${printLogo}</div>
@@ -288,54 +311,16 @@ $(document).ready(() => {
          </div>
         </div>
         `
-        console.log(html)
+        html += templateHtml
       })
-
-        // //cache elements from commendation to print
-        // // const id = $elem.attr('data-id')
-        // //format name and class
-        // const name = $(this).find('.commendation-name').html()
-        // const schoolClass = $(this).find('.commendation-class').html()
-        // //slice brackets off date
-        // const date = $(this).find('.commendation-date').html().slice(1, -1)
-        // //get reason and author
-        // const reason = $(this).find('.commendation-reason').html()
-        // //slice off the word 'by'
-        // const by = $(this).find('.commendation-by').html().slice(2)
-        // const printLogo = logoTemplate('#000000', 100)
-        // //render template
-        // const html = `
-        // <div class="printing" id="printing-${id}">
-        //   <div class="banner">
-        //     <div class="logo-left">${printLogo}</div>
-        //       <h2>Decoy Community Primary School</h2>
-        //       <h2>Certificate of Commendation</h2>
-        //     <div class="logo-right">${printLogo}</div>
-        //     <p>This certificate is awarded to</p>
-        //     <h1>${name}</h1>
-        //     <p>in ${schoolClass}</p>
-        //   </div>
-        //     <p>For ${reason}</p>
-        //     <p>Nominated by ${by}, ${date}</p>
-        //     <p>Signed:</p>
-        //   <div class="sig-box">
-        //     <img src="./images/sig-temp.png">
-        //     <p>Mrs G O'Neill, headteacher</p>
-        //   </div>
-        // </div>
-        // `
-        // return html
-
-
-      // $commendationsContainer.append(html)
-      // //set timeout is a hack to make sure imgs in the printed template are
-      // //loaded. can be removed once inline svgs are in place.
-      // setTimeout(() => {
-      //   window.print()
-      //   $('.printing').remove()
-      // }, 250)
-
-  }
+      $commendationsContainer.append(html)
+      //set timeout is a hack to make sure imgs in the printed template are
+      //loaded. can be removed once inline svgs are in place.
+      setTimeout(() => {
+        window.print()
+        $('.printing').remove()
+      }, 250)
+    }
 
     //called by delete buttons (when children are removed from the db ref)
     function removeOneCommendation(snapshot){
@@ -343,12 +328,17 @@ $(document).ready(() => {
       $divToRemove.slideUp(300, () => {$(this).remove()})
     }
 
-    //called on logout
+    //called on logout by change in auth state
+    function removeControlsForm($container){
+      $($container).html('').addClass('hidden')
+    }
+
+    //called on logout by change in auth state
     function removeAllCommendations($container){
       $container.find('div').remove()
     }
 
-    // returns inline svg of logo, supply size in px and colour as #
+    //returns inline svg of logo, supply size in px and colour as #
     function logoTemplate(colour, size){
       return `<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 570 570"><path d="m410.2 12.6c13.4-18.8 2.2 23.7 12.8 17 0.5 10.1-21.3 39.9 1.8 26.6 3.8-21.6 16.9-8.1 7.1 5.5-10.4 25.9-48.5 23.7-56.6 51.2-0.6 16.9 26.3 21.9 35.5 36 11 5.8 38 6.8 24.3 25.4-17.7 8.6-37.8 12.7-39.3 36.8-7.9 19.3-9.4 41.9 0.5 60.5 11.5 19.8-10.9 59.5 23.4 61.6 18.8-5 53.6 0.1 44.7 27.1-13.4 17.9-11.6 43.1-27.6 58.8-9.1 9.6-35.9 24.8-27.4-0.5 1.8-13.5 21-19 14-37.5-1.1-29.4-31.4 5.6-46.2 7.6-20.9 5-42.3 4.7-63.5 6.2-25.7 0.7-52.8-2.6-77.1 7.2-22.3 11.3-30.5 36.9-51.5 49.2-18.6 14.9-51.3 8.7-61.9 32.9 0.2 29.1-11.1 53.9-36 69.5-16.2 17.3-18.1-27.6 0.9-25.1 18.4-14.6 13.6-40.5 16.9-61.1 6.4-24.9 38.3-34.9 40.7-63.1 4.1-18.9-3.1-38.8 2.7-58 4.1-15.6-23.8-34.5 5.4-34.6 11.1 0 13.7 17.6 27 4.6 26.3-6.3 54.1-2 80-10.9 32.9-11.4 61.1-36.6 74.2-69.1 14-26.7 17-58.2 15.5-87.9-18.1-6.1-36.9-55-5.3-40.2 21.1 21.4 6.8-25.2-1.6-33.7-12.4-22.5 8.9-6.1 14.6 4.1 23.5 2.8 21.8-32.5 24.8-48.9 12.1-11.5 5.8 38.6 15.8 14.5 6.7-9.3 10.2-20.5 11.4-31.8z" fill="${colour}"/></svg>`
     }
